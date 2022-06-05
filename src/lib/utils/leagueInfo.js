43f51b6c9ee07f46dc14aac116b11ca3c59bce8e
cheezytaterz <104937666+cheezytaterz@users.noreply.github.com>
2022-06-05 12:46:07 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -53,7 +53,7 @@ export const managers = [
       "location": "Orlando", // (optional)
       "bio": "I love getting freaky in fantasy.",
       "photo": "/managers/mac.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2018, // (optional) when did the manager start playing fantasy football
+      "fantasyStart": 2016, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "buf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
